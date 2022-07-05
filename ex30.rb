70ebe580f90bb073fc235d9e37f73d8efa2476a1
Kat Brandt <joyfulkat11@gmail.com>
2022-07-05 17:16:48 -0600

    Add ex30

---
+++ ex30.rb
@@ -0,0 +1,15 @@
+people = 30
+cars = 40
+trucks = 15
+
+people -= 10
+
+
+
+if cars > people
+  puts "We should take the cars."
+elsif cars < people
+  puts "We should nt take the cars."
+else
+  puts "We can't decide."
+end
